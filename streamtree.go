763c273bb13f81ecef3f70900a50f392e1929a0f
JerryLX <724733136@qq.com>
2020-01-29 11:28:42 +0800

    use pointer

--- streamtree.go
+++ streamtree.go
@@ -8,12 +8,12 @@ type StreamTree struct {
 	bsrv blockservice.BlockService
 }
 
-func (st *StreamTree) StartStream(s Stream) error {
-
+func (st *StreamTree) StartStream(s *Stream) error {
+    return nil
 }
-func (st *StreamTree) AddStreamBlock(b StreamBlock) error {
-
+func (st *StreamTree) AddStreamBlock(b *StreamBlock) error {
+    return nil
 }
-func (st *StreamTree) SubscribeStream(conf StreamConfig) error {
-
+func (st *StreamTree) SubscribeStream(conf *StreamConfig) error {
+    return nil
 }
