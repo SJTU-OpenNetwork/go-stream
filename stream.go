db420923dc0874d9fc46b1099cf480cf4428b815
Riften <943275366@qq.com>
2020-02-18 03:42:29 +0800

    Add interface

--- stream.go
+++ stream.go
@@ -2,12 +2,15 @@ package stream
 
 import (
 	"github.com/ipfs/go-cid"
+	ipld "github.com/ipfs/go-ipld-format"
 )
 
-type Stream struct {
-	ID          uint64  // hash value of the stream
-	Rate        float32 // in Kbps
-	Nsubstreams int     // number of substreams
+type Stream interface {
+	AddJob(ipld.Node) error
+	//GetAll(func(*StreamBlock)) error
+	//Subscribe(callback func(*ipld.Node))
+	//Unsubscribe()
+	StartWorker() error
 }
 
 type StreamBlock struct {
@@ -15,7 +18,20 @@ type StreamBlock struct {
 	BlockID  cid.Cid
 }
 
+
+/*
+ * Used to store streamblocks within a stream.
+ */
+type blocklist struct {
+
+}
+
 type StreamConfig struct {
+	StreamID uint64
+	Nsubstreams int
+}
+
+type SubStreamConfig struct {
 	ID         uint64
 	StreamMap  uint64 // 0010 means need only the second sub-stream
 	StartIndex int    // only download blocks after StartIndex
---
+++ videostream.go
@@ -0,0 +1,29 @@
+package stream
+
+import (
+ipld "github.com/ipfs/go-ipld-format"
+)
+
+/*
+ * VideoStream is one implementation of stream for videos.
+ */
+type VideoStream struct {
+	ID          uint64  // hash value of the stream
+	//Rate        float32 // in Kbps
+	Nsubstreams int     // number of substreams
+	blocklist   *blocklist	//Used to store blocks
+}
+
+/*
+ * Add a video chunk into video stream.
+ */
+func (s *VideoStream) AddFile(nd ipld.Node) error {
+	return nil
+}
+
+func NewVideoStream(c *StreamConfig) *VideoStream {
+	return &VideoStream{
+		ID: c.StreamID,
+		Nsubstreams: c.Nsubstreams,
+	}
+}
