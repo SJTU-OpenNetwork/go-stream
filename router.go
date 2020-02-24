b984eb02fcca55c636f82e312c014584ca2e7098
JerryLX <724733136@qq.com>
2020-02-24 17:52:16 +0800

    add StartWorker

--- router.go
+++ router.go
@@ -2,10 +2,12 @@ package stream
 
 import (
     "context"
+    peer "github.com/libp2p/go-libp2p-core/peer"
 )
 
 type Router interface {
 	StartStream(context.Context, *Stream) error
 	AddStreamBlock(context.Context, *StreamBlock) error
 	SubscribeStream(context.Context, *StreamConfig) error
+	StartWorker(context.Context, *StreamConfig, peer.ID) error
 }
--- streamtree.go
+++ streamtree.go
@@ -3,6 +3,7 @@ package stream
 import (
     "context"
 	"github.com/ipfs/go-blocks/blockservice"
+    peer "github.com/libp2p/go-libp2p-core/peer"
 )
 
 type StreamTree struct {
@@ -16,5 +17,10 @@ func (st *StreamTree) AddStreamBlock(ctx context.Context, b *StreamBlock) error 
     return nil
 }
 func (st *StreamTree) SubscribeStream(ctx context.Context, conf *StreamConfig) error {
+    return nil
+}
+func (st *StreamTree) StartWorker(ctx context.Context, conf *StreamConfig, pid peer.ID) error {
+    //stream := getStream(conf.ID)
+    //call stream.StartWorker
     return nil
 }
